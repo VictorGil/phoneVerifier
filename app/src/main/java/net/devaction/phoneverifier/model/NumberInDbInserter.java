a94f788e6f34d8d6c8f9421cba9a68c320d0eede
victorGil <victorgilsanz@gmail.com>
2017-09-10 20:27:10 +0200

    Adding logic to NumberInDbInserter

--- app/src/main/java/net/devaction/phoneverifier/model/NumberInDbInserter.java
+++ app/src/main/java/net/devaction/phoneverifier/model/NumberInDbInserter.java
@@ -1,12 +1,27 @@
 package net.devaction.phoneverifier.model;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import net.devaction.phoneverifier.model.util.DatabaseProvider;
 
 /**
  * @author Víctor Gil
  */
 public class NumberInDbInserter{
+    private static final String TAG = "NumberInDbInserter";
+
     public static void insert(Context context, String phoneNumber){
-        //TO DO
+        SQLiteDatabase database = DatabaseProvider.provideWritableDatabase(context);
+        database.insert(UserTable.NAME, null, provideContentValues(phoneNumber));
+        Log.d(TAG, "User phone numbner " + phoneNumber + " has been inserted into the database");
+    }
+
+    private static ContentValues provideContentValues(String phoneNumber){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(UserTable.Cols.PHONE_NUMBER, phoneNumber);
+        return contentValues;
     }
 }
